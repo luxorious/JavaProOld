fc59c75a45d8ef9ce5c9fa2088fc1ccd5bddb42e
luxorious <vovan_194@mail.ru>
2022-12-19 19:58:34 +0100

    1

--- lesons/lesson5/src/printArr/CreateArray.java
+++ lesons/lesson5/src/printArr/CreateArray.java
@@ -5,12 +5,11 @@ import java.util.Random;
 public class CreateArray {
     private int lengthArr;
     private int[] arr = new int[lengthArr];
+    Random random = new Random();
 
     public CreateArray(int lengthArr) {
         this.lengthArr = lengthArr;
     }
-
-    Random random = new Random();
 
     public int[] create(){
         for (int i = 0; i < arr.length; i++) {
