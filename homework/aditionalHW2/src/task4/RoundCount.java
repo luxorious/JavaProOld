b7df80960afaccabacd46ecef2fccdd994eeae2b
luxorious <vovan_194@mail.ru>
2022-12-23 20:31:31 +0100

    delete not used files

---
+++ homework/aditionalHW2/src/task4/RoundCount.java
@@ -0,0 +1,20 @@
+package task4;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RoundCount {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input radius");
+        int r = scanner.nextInt();
+
+        double p = 2 * Math.PI * r;
+        double s = Math.PI * (r * r);//Math.sqrt(r);
+
+        System.out.printf("Perimeter is - %.2f\n", p);
+        System.out.printf("Area is - %.2f", s);
+    }
+}
---
+++ lesons/lesson6/src/colections/ArrayList/PersonDBArrayListDemo.java
@@ -0,0 +1,4 @@
+package colections.ArrayList;
+
+public class PersonDBArrayListDemo {
+}
