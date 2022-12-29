fb78407519851f3602cc86142edf24d86d957918
luxorious <vovan_194@mail.ru>
2022-12-29 19:13:48 +0100

    delete coments

--- homework/Arrays/src/Array2D/PrintArrayInConsole.java
+++ homework/Arrays/src/Array2D/PrintArrayInConsole.java
@@ -2,14 +2,11 @@ package Array2D;
 
 public class PrintArrayInConsole {
     public void printTwoDimensionalArray (int[][] arr){
-//        int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
                 System.out.print(arr[i][j] + " ");
-//                sum+=arr[i][j];
             }
             System.out.println();
         }
-//        System.out.println(sum);
     }
 }
