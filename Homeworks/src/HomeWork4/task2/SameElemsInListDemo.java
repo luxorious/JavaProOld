8060e54851fc33b5ba9dacf62e32d48311b5c609
luxorious <vovan_194@mail.ru>
2023-01-15 17:47:13 +0100

    HW4

---
+++ Homeworks/src/HomeWork4/task2/SameElemsInListDemo.java
@@ -0,0 +1,26 @@
+package HomeWork4.task2;
+
+import HomeWork4.task1.IntegerListDemo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//        Могут ли реализации интерфейса java.util.List хранить одинаковые
+//        элементы (дубликаты)? Напишите код программы, который демонстрирует ответ на этот вопрос.
+public class SameElemsInListDemo {
+    public static void main(String[] args) {
+        IntegerListDemo i = new IntegerListDemo();
+
+        ArrayList<IntegerListDemo> li = new ArrayList<>();
+        li.add(i);
+        li.add(i);
+        System.out.println(li);
+
+        List<Integer> example = new ArrayList<>();
+        int a = 10;
+        example.add(a);
+        example.add(a);
+
+        System.out.println(example);
+    }
+}
